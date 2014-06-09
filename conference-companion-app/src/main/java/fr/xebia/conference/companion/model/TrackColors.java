2feb33429ed44523ad1160302023bcc0ab8ff5e4
blacroix <benjlacroix@gmail.com>
2014-06-09 11:44:32 +0200

    Update colors

--- conference-companion-app/src/main/java/fr/xebia/conference/companion/model/TrackColors.java
+++ conference-companion-app/src/main/java/fr/xebia/conference/companion/model/TrackColors.java
@@ -12,15 +12,15 @@ public class TrackColors {
     public final static List<Integer> LIST = Arrays.asList(
             Color.parseColor("#00A3C1"),
             Color.parseColor("#0061B5"),
-            Color.parseColor("#0012A5"),
-            Color.parseColor("#6300A5"),
+            Color.parseColor("#0FAD00"),
+            Color.parseColor("#E266B4"),
             Color.parseColor("#C5007C"),
             Color.parseColor("#FF0000"),
             Color.parseColor("#FF6600"),
-            Color.parseColor("#FF9400"),
+            Color.parseColor("#888888"),
             Color.parseColor("#FFC600"),
             Color.parseColor("#FFFF00"),
             Color.parseColor("#8CC700"),
-            Color.parseColor("#0FAD00")
+            Color.parseColor("#0012A5")
     );
 }
