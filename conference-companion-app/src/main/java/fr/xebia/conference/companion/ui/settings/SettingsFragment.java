f24f6f0a6dc786bfa3f736c99c419f86bb53d27d
Thomas Guerin <thomas.guerin2@gmail.com>
2014-06-10 02:21:40 +0200

    Handle preference changes

--- conference-companion-app/src/main/java/fr/xebia/conference/companion/ui/settings/SettingsFragment.java
+++ conference-companion-app/src/main/java/fr/xebia/conference/companion/ui/settings/SettingsFragment.java
@@ -1,12 +1,14 @@
 package fr.xebia.conference.companion.ui.settings;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import fr.xebia.conference.companion.R;
+import fr.xebia.conference.companion.service.BluetoothLocationIntentService;
 import timber.log.Timber;
 
 public class SettingsFragment extends PreferenceFragment {
@@ -19,14 +21,22 @@ public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Activity activity = getActivity();
+        final Activity activity = getActivity();
         addPreferencesFromResource(R.xml.settings);
         try {
             PackageInfo pInfo = activity.getPackageManager().getPackageInfo(activity.getPackageName(), 0);
-            Preference versionPref = (Preference) findPreference(KEY_VERSION);
+            Preference versionPref = findPreference(KEY_VERSION);
             versionPref.setSummary(pInfo.versionName);
         } catch (PackageManager.NameNotFoundException e) {
             Timber.d(e, "Couldn't get package name");
         }
+
+        findPreference(KEY_PREF_BLE_LOCATION).setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    activity.startService(new Intent(activity, BluetoothLocationIntentService.class));
+                return true;
+            }
+        });
     }
 }
