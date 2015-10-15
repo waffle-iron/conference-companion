0b8bbffd2240450ef27a6948de162384379244dc
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2015-10-15 19:57:55 +0200

    Add swipe refresh for tweet listing

--- conference-companion-app/src/main/java/fr/xebia/xebicon/ui/timeline/TimelineActivity.java
+++ conference-companion-app/src/main/java/fr/xebia/xebicon/ui/timeline/TimelineActivity.java
@@ -1,6 +1,7 @@
 package fr.xebia.xebicon.ui.timeline;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -12,10 +13,11 @@ import fr.xebia.xebicon.R;
 import fr.xebia.xebicon.core.activity.NavigationActivity;
 import fr.xebia.xebicon.ui.widget.ObservableListView;
 
-public class TimelineActivity extends NavigationActivity {
+public class TimelineActivity extends NavigationActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     @InjectView(android.R.id.list) ObservableListView listView;
     @InjectView(android.R.id.empty) TextView emptyView;
+    @InjectView(R.id.swipe_refresh) SwipeRefreshLayout swipeRefreshLayout;
 
     public TimelineActivity() {
         super(R.layout.timeline_activity);
@@ -27,15 +29,29 @@ public class TimelineActivity extends NavigationActivity {
 
         setTitle(R.string.nav_timeline);
 
+        refreshListing();
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+    }
+
+    private void refreshListing() {
         final SearchTimeline searchTimeline = new SearchTimeline.Builder()
                 .query("#XebiconFr OR @XebiaFr")
                 .build();
         listView.setAdapter(new TweetTimelineListAdapter(this, searchTimeline));
         listView.setEmptyView(emptyView);
+
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
     protected int getNavId() {
         return R.id.nav_timeline;
+    }
+
+    @Override
+    public void onRefresh() {
+        swipeRefreshLayout.setRefreshing(true);
+        refreshListing();
     }
 }
