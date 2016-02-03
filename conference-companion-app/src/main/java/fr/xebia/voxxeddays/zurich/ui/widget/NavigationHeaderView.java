b98440bc746c9d7d58f2b45d4ab51dc3ec936c21
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2016-02-03 10:49:08 +0100

    Remove renderscript usage

--- conference-companion-app/src/main/java/fr/xebia/voxxeddays/zurich/ui/widget/NavigationHeaderView.java
+++ conference-companion-app/src/main/java/fr/xebia/voxxeddays/zurich/ui/widget/NavigationHeaderView.java
@@ -2,17 +2,11 @@ package fr.xebia.voxxeddays.zurich.ui.widget;
 
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.support.v8.renderscript.Allocation;
-import android.support.v8.renderscript.Element;
-import android.support.v8.renderscript.RenderScript;
-import android.support.v8.renderscript.ScriptIntrinsicBlur;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Transformation;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -50,52 +44,9 @@ public class NavigationHeaderView extends RelativeLayout {
         Conference conference = KouignAmanApplication.getGson().fromJson(SynchroIntentService.VOXXEDDAYS_ZURICH_CONFERENCE, Conference.class);
         Picasso.with(getContext())
                 .load(conference.getBackgroundUrl())
-                .transform(new BlurTransformation(getContext()))
-
                 .fit()
                 .centerCrop()
                 .into(background);
     }
 
-    public class BlurTransformation implements Transformation {
-
-        RenderScript rs;
-
-        public BlurTransformation(Context context) {
-            super();
-            rs = RenderScript.create(context);
-        }
-
-        @Override
-        public Bitmap transform(Bitmap bitmap) {
-            // Create another bitmap that will hold the results of the filter.
-            Bitmap blurredBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-
-            // Allocate memory for Renderscript to work with
-            Allocation input = Allocation.createFromBitmap(rs, blurredBitmap, Allocation.MipmapControl.MIPMAP_FULL, Allocation.USAGE_SHARED);
-            Allocation output = Allocation.createTyped(rs, input.getType());
-
-            // Load up an instance of the specific script that we want to use.
-            ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
-            script.setInput(input);
-
-            // Set the blur radius
-            script.setRadius(20);
-
-            // Start the ScriptIntrinisicBlur
-            script.forEach(output);
-
-            // Copy the output to the blurred bitmap
-            output.copyTo(blurredBitmap);
-
-            bitmap.recycle();
-
-            return blurredBitmap;
-        }
-
-        @Override
-        public String key() {
-            return "blur";
-        }
-    }
 }
